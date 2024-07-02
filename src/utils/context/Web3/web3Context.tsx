4aa98b9fc535c861e1c6c93e6622cf060105968f
tomvictor303 <tomvictor303@gmail.com>
2024-07-01 20:11:27 -0400

    remove useless web3Context functions

--- src/utils/context/Web3/web3Context.tsx
+++ src/utils/context/Web3/web3Context.tsx
@@ -10,7 +10,6 @@ import createWeb3ModalInstance from './web3modal'
 import { showSnackBar, hideSnackBar } from 'src/store/slices/snackbar.slice';
 import { showBackdrop, hideBackdrop } from 'src/store/slices/backdrop.slice';
 import { useAppDispatch, useAppSelector } from 'src/store/hooks';
-import { useUser } from '../User/UserProvider'
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const MAIN_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_MAIN_CONTRACT_ADDRESS;
@@ -28,8 +27,6 @@ export const Web3Provider = ({ children }: any) => {
   // https://docs.walletconnect.com/web3modal/nextjs/hooks?platform=ethers
   const { address, chainId, isConnected } = useWeb3ModalAccount()
 
-  const { user, setUser, setJwtToken } = useUser();
-
   useEffect(() => {
     console.log('BEGIN Web3Provider Init ----------------------------------------------------');
     setWeb3ModalInstance(createWeb3ModalInstance());
@@ -43,61 +40,12 @@ export const Web3Provider = ({ children }: any) => {
 
   const wallet_account_change_handler = async ( address: any, chainId: number | undefined, isConnected: boolean, walletProvider: any ) => {
     if (!address || !walletProvider) { return; }
-    if ( user ) { return; }
 
     return; // Nothing to do now.
   }
 
-  const web3_createJob = async ( jobId: number, freelancer_wallet: string, budget: number ) => {
-    if (!address || !walletProvider || !isConnected || !MAIN_CONTRACT_ADDRESS || !TOKEN_CONTRACT_ADDRESS) {     
-      return dispatch(showSnackBar({ type: 'error', message: `Wallet is not connected` })); 
-    }
-
-    const provider = new BrowserProvider(walletProvider)
-    const signer = await provider.getSigner()
-
-    // const mainContract = new ethers.Contract(MAIN_CONTRACT_ADDRESS, FreelancePlatform.abi, signer);
-    // try {
-    //   const transaction = await mainContract.createJob(jobId, freelancer_wallet, ethers.parseEther(budget.toString()));
-    //   const receipt = await transaction.wait(); // Wait for transaction to be mined
-
-    //   console.log('Transaction receipt:', receipt);
-    // } catch(error: any) {      
-    //   console.log(error);
-    //   dispatch(hideBackdrop(null));
-    //   if ( error?.action === "estimateGas") {
-    //     return dispatch(showSnackBar({ type: 'error', message: `${error.reason || error.toString()}` })); 
-    //   }
-    //   if ( error?.action === "signMessage" && error?.code === "ACTION_REJECTED" ) {
-    //     return dispatch(showSnackBar({ type: 'error', message: `You rejected web3 action!` })); 
-    //   }
-    //   return dispatch(showSnackBar({ type: 'error', message: `${error.message || error.toString()}` })); 
-    // }
-    
-  }
-
-  const web3_get_allowance = async (): Promise<number> => {
-    if (!address || !walletProvider || !isConnected || !MAIN_CONTRACT_ADDRESS || !TOKEN_CONTRACT_ADDRESS) {      
-      throw new Error(`Wallet is not connected`); 
-    }
-
-    const provider = new BrowserProvider(walletProvider)
-    const signer = await provider.getSigner()
-
-    // const tokenContract = new ethers.Contract(TOKEN_CONTRACT_ADDRESS, AskToken.abi, signer);
-    // try {
-    //   const allowance = await tokenContract.allowance(address, MAIN_CONTRACT_ADDRESS);
-    //   console.log(`allowance`, allowance)
-    //   return allowance;
-    // } catch(error: any) {
-    //   throw error;
-    // }
-    
-    return 0;
-  }
-
   return (
-    <Web3Context.Provider value={{ web3ModalInstance, web3_createJob, web3_get_allowance }}>
+    <Web3Context.Provider value={{ web3ModalInstance }}>
       {children}
     </Web3Context.Provider>
   )
